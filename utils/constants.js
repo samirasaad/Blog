1ca058e807e2c5b8b11322659a97160514006897
Samira <samirasaad@gmail.com>
2021-04-01 11:41:13 +0200

    change app domain

--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,5 @@ const ID = "id";
 const USERS = "Users";
 const ARTICLES = "Articles";
 const FAVORITES = "Favorites";
-const BASE_HREF = "https://blog-steel-nu.vercel.app";
+const BASE_HREF = "https://blog-samirasaad.vercel.app";
 export { ID, USERS, ARTICLES, FAVORITES, BASE_HREF };
