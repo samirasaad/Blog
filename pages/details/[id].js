15bc1556dda117234a0a24d753ba1ca8b1495e56
Samira <samirasaad@gmail.com>
2021-06-08 10:43:23 +0200

    reading article info inside getintialprops

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -8,30 +8,31 @@ import AuthorInfo from "../../components/AuthorInfo/AuthorInfo";
 import HeadSection from "../../components/HeadSection/HeadSection";
 import LoaderComp from "../../components/Loader/Loader";
 import ArticleCover from "../../components/ArticleCover/ArticleCover";
+import Head from "next/head";
 
-const articleDetails = ({ id }) => {
+const articleDetails = ({ articleInfo }) => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [articleID, setArticleID] = useState(null);
-  const [articleInfo, setArticleInfo] = useState(null);
+  // const [articleInfo, setArticleInfo] = useState(null);
 
   useEffect(() => {
     Cookies.get("userInfo") &&
       setUser(JSON.parse(Cookies.get("userInfo")) || null);
   }, []);
 
-  useEffect(() => {
-    id && setArticleID(id);
-  }, [id]);
+  // useEffect(() => {
+  //   id && setArticleID(id);
+  // }, [id]);
 
-  useEffect(() => {
-    let mounted = true;
-    if (mounted && articleID) {
-      articleID && getArticleDetails();
-    }
-    return () => (mounted = false);
-  }, [articleID]);
+  // useEffect(() => {
+  //   let mounted = true;
+  //   if (mounted && articleID) {
+  //     articleID && getArticleDetails();
+  //   }
+  //   return () => (mounted = false);
+  // }, [articleID]);
 
   const getArticleDetails = () => {
     setLoading(true);
@@ -51,10 +52,18 @@ const articleDetails = ({ id }) => {
         }
       );
   };
-
+  console.log(articleInfo);
   return (
     <section className="section-min-height">
-      <HeadSection
+      <Head>
+        <title>{`Blog | ${articleInfo && articleInfo.title}`}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={articleInfo.title} />
+        {/* <meta property="og:description" content={productDetails.description} /> */}
+        <meta property="og:image" content={articleInfo.coverImg} />
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+      </Head>
+      {/* <HeadSection
         title={`Blog | ${articleInfo && articleInfo.title}`}
         metadata={[
           {
@@ -73,7 +82,7 @@ const articleDetails = ({ id }) => {
           { name: "author", content: "Samira Saad" },
         ]}
         links={[{ rel: "icon", href: "/favicon.ico" }]}
-      />
+      /> */}
       {articleInfo && !loading ? (
         <>
           {/* ARTICLE COVER */}
@@ -104,9 +113,72 @@ const articleDetails = ({ id }) => {
 
 export default articleDetails;
 
-articleDetails.getInitialProps = async (ctx) => {
+articleDetails.getInitialProps = async function (ctx) {
   const id = ctx.query.id;
+  // let d = {};
+  // console.log(id);
+  // let x = await db
+  //   .collection(ARTICLES)
+  //   .where("id", "==", id)
+  //   .onSnapshot(
+  //     (querySnapshot) => {
+  //       d = querySnapshot.docs.map((doc) => {
+  //         return doc.data();
+  //       });
+  //       d = articleDetails[0];
+  //       // console.log(articleDetails)
+  //       // console.log(articleDetails)
+  //       // setArticleInfo(articleDetails[0]);
+  //       // setLoading(false);
+  //     },
+  //     (error) => {
+  //       // setLoading(false);
+  //       console.log(error);
+  //     }
+  //   );
+  // console.log(d);
+
+  let articleInfo = [];
+  const querySnapshot = await db.collection("Articles").get();
+  querySnapshot.forEach((doc) => {
+    articleInfo.push(doc.data());
+  });
+
   return {
-    id,
+    articleInfo: articleInfo.find((article) => article.id === id),
   };
 };
+
+// articleDetails.getInitialProps = async (ctx) => {
+//   const id = ctx.query.id;
+//   console.log(id);
+//   let articleDetails = {};
+//   const getArticleDetails = async () => {
+//     // setLoading(true);
+//    return await db
+//       .collection(ARTICLES)
+//       .where("id", "==", id)
+//       .onSnapshot(
+//         (querySnapshot) => {
+//            articleDetails = querySnapshot.docs.map((doc) => {
+//             return doc.data();
+//           });
+//           // d = articleDetails[0];
+//           // console.log(articleDetails)
+//           // console.log(articleDetails[0])
+//           // setArticleInfo(articleDetails[0]);
+//           // setLoading(false);
+//         },
+//         (error) => {
+//           // setLoading(false);
+//           console.log(error);
+//         }
+//       );
+//   };
+//  let x = getArticleDetails()
+//   console.log(x)
+//   console.log(articleDetails)
+//   return {
+//     articleInfo: articleDetails,
+//   };
+// };
